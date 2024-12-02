5a79cc0a8b1d727e2088104d261aa4bd9489518e
BMilliet <brunomilliet89@gmail.com>
2024-12-02 09:06:33 +0000

    basic customization

--- SampleApp/SampleApp/ViewController.swift
+++ SampleApp/SampleApp/ViewController.swift
@@ -2,11 +2,24 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private lazy var customLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        self.view.backgroundColor = .cyan
+        self.view.addSubview(customLabel)
+
+        NSLayoutConstraint.activate([
+            customLabel.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            customLabel.centerYAnchor.constraint(equalTo: self.view.centerYAnchor),
+        ])
+
+        customLabel.text = "Hello, World!"
     }
-
-
 }
 
